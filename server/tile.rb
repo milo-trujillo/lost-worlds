5c78879af47136ec21e8c631f60cfc6aa51780fc
Milo Trujillo <milo.trujillo@sonomaacademy.org>
2015-05-16 01:17:22 -0400

    Added more tile utility code

--- server/tile.rb
+++ server/tile.rb
@@ -26,6 +26,36 @@ class Tile
 
 end
 
+# Calculates the coordinates and verices overlapping a particular tile/vertex
+def adjoiningTiles(tile, vertex)
+	if( tile.is_a?(Tile) && vertex.is_a?(Integer) )
+		case vertex
+			when 0
+				return [[tile.row - 1,tile.col - 1, 2],
+					[tile.row - 1, tile.col, 4]]
+			when 1
+				return [[tile.row - 1, tile.col, 3],
+					[tile.row, tile.col + 1, 5]]
+			when 2
+				return [[tile.row, tile.col + 1, 4],
+					[tile.row + 1, tile.col, 0]]
+			when 3
+				return [[tile.row + 1, tile.col - 1, 1],
+					[tile.row + 1, tile.col, 5]]
+			when 4
+				return [[tile.row, tile.col - 1, 2],
+					[tile.row + 1, tile.col - 1, 0]]
+			when 5
+				return [[tile.row - 1, tile.col - 1, 3],
+					[tile.row, tile.col - 1, 1]]
+			else
+				raise "Invalid vertex!"
+		end
+	else
+		raise "Expected a tile and an integer vertex"
+	end
+end
+
 def getTotalTiles()
 	total = 0
 	for x in Boardsize
