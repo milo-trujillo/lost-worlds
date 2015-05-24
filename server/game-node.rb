295b4f640bfbe6bd267911ea12bb659d0e832304
Milo Trujillo <milo.trujillo@sonomaacademy.org>
2015-05-23 20:01:41 -0400

    Split receiving orders and executing them into seperate modules

--- server/game-node.rb
+++ server/game-node.rb
@@ -14,6 +14,8 @@ require_relative 'network'
 require_relative 'random'
 require_relative 'user'
 require_relative 'tile'
+require_relative 'world'
+require_relative 'log'
 
 #
 # Global constants
@@ -24,94 +26,42 @@ DenyOrder = "Order rejected"
 #
 # Global variables
 #
-$board = [] # Stores the actual board data
 $orders = [] # Stores orders between turn executions
 $boardlock = Mutex.new # For freezing all orders while we update board
 $turn = 1
 
-#
-# We want a fairly even distribution of resources across the continent
-# We also want a fairly even distribution of dice rolls for our tiles.
-# We'll get this by generating numbers from the center of 2..12 outwards
-# in both directions, repeated until we run out of tiles.
-#
-# Note to self: Start at die roll 7 and work outwards
-#
-def initBoard()
-	tilepool = []
-	tiledistribution = (getTotalTiles() / Tiletypes.length).to_i
-	for type in Tiletypes
-		for x in (1 .. tiledistribution)
-			tilepool.push(type)
-		end
-	end
-
-	probpool = []
-	diff = 0
-	while( probpool.length() < getTotalTiles() )
-		probpool.push(Midroll + diff)
-		if( probpool.length < getTotalTiles() && diff != 0 )
-			probpool.push(Midroll - diff)
-		end
-		diff += 1
-		if( 7 + diff > Maxroll )
-			diff = 0
-		end
-	end
-
-	for r in 0 .. (Boardsize.length - 1)
-		row = []
-		for x in 0 .. (Boardsize[r] - 1)
-			type = ""
-			if( tilepool.length > 0 )
-				type = extractRandomElement(tilepool)
-			else
-				type = getRandomElement(Tiletypes)
-			end
-			prob = extractRandomElement(probpool)
-			row.push(Tile.new(type, r, x, prob))
-		end
-		$board.push(row)
-	end
-end
-
-def log(s)
-	puts(s)
-end
-
-def getBoardDescription()
-	log("Returning board description")
-	descr = []
-	for row in 0 .. ($board.length - 1)
-		for col in 0 .. ($board[row].length - 1)
-			descr.push($board[row][col].getIDString())
-		end
-	end
-	return descr
-end
-
 def attemptBuild(info)
+	# info is in format build:type:row:col:vertex
 	log("Received build order")
-	return ConfirmOrder
+	userRow = info[2]
+	userCol = info[3]
+	userVertex = info[4]
+	if( validTile?(userRow, userCol, userVertex) )
+		$oders.push(info) # TODO: Look into Ruby hash tables and clean up orders
+		log("Order accepted")
+		return ConfirmOrder
+	else
+		return (DenyOrder + ": Tile location invalid")
+	end
 end
 
 def handleConnection(conn)
 	command = conn.gets.chomp()
-	boardlock.synchronize {
+	response = []
+	$boardlock.synchronize {
 		case command
 			when /^description$/
 				response = getBoardDescription()
-				for r in response
-					conn.puts(r)
-				end
 			# build:building_type:row:column:vertex
 			when /^build:[\w]+:[\d]+:[\d]+:[\d]+$/
-				response = attemptBuild(command.split(':')[1..4])
-				conn.puts(response)
+				response.push(attemptBuild(command.split(':')))
 			else
 				log("Received unknown command from master: " + command)
 		end
 	}
+	for r in response
+		conn.puts(r)
+	end
 	conn.close()
 end
 
@@ -132,6 +82,8 @@ if __FILE__ == $0
 		sleep(Turnduration)
 		$boardlock.synchronize {
 			log("Turn heartbeat: " + $turn.to_s)
+			updateBoard($orders)
+			$orders = []
 			$turn += 1
 		}
 	end
