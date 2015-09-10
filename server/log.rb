75afee421666434a1cff9375d2c89144551f609f
Milo Trujillo <milo.trujillo@sonomaacademy.org>
2015-09-10 18:28:02 -0400

    Overhauled logging system

--- server/log.rb
+++ server/log.rb
@@ -1,15 +1,54 @@
 #!/usr/bin/env ruby
 
 =begin
-This file handles logging status and debug messages for the master and game
-nodes.
+	This file defines how messages should be logged.
 =end
 
-require_relative 'config'
+require 'thread'
 
-# For now logging consists of dumping to a terminal, but we may later
-# want a more detailed log file, or log over the network to a centralized
-# location. This makes it easy to transition later.
-def log(s)
-	puts(s)
+module Log
+	$logLock = Mutex.new
+	$debugMode = true
+
+	Debug   = 1
+	Info    = 2
+	Warning = 3
+	Error   = 4
+
+	def Log.log(level, msg)
+		case level
+			when Debug
+				if( $debugMode == true )
+					$logLock.synchronize {
+						puts "Debug: " + msg.to_s
+					}
+				end
+			when Info
+				$logLock.synchronize {
+					puts "Info: " + msg.to_s
+				}
+			when Warning
+				$logLock.synchronize {
+					puts "WARNING: " + msg.to_s
+				}
+			when Error
+				$logLock.synchronize {
+					puts "ERROR: " + msg.to_s
+				}
+			else
+				$logLock.synchronize {
+					Log.log(Warning, "Log message has improper log level")
+				}
+		end
+	end
+
+	# If we want to set debug mode based on a command line argument...
+	def Log.setDebugMode(bool)
+		# Only want to set valid booleans
+		if( bool == true )
+			$debugMode = true
+		elsif( bool == false )
+			$debugMode = false
+		end
+	end
 end
